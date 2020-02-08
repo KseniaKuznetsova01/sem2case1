7dd5b9eb2ed2968b38962cb71de6f6d50146d256
KseniaKuznetsova01 <ksenks.one@gmail.com>
2020-02-08 15:34:30 +0700

    part 1

--- case1.py
+++ case1.py
@@ -1,5 +1,14 @@
 import turtle
-
+print('''Select a fractal:
+1. The Koch Curve
+2. Koch's Snowflake.
+3. Ice fractal (example 1)
+4. Ice fractal (example 2)
+5. Snowflake to an ice fractal (example 1) with the construction of external and internal fractals.
+6. A snowflake to an ice fractal (example 2) with the construction of the outer and inner fractals.
+7. Minkowski Curve
+8. Levi's Curve
+9. Building a binary tree.''')
 
 def koch(order, size):
     if order == 0:
